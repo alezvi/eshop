04f480d9a2956a3a5c9acafb70cd5a9361ba6f56
alezvi <alezvi@users.noreply.github.com>
2017-08-25 07:53:07 -0300

    Added attributes, setters and getters.

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -1,3 +1,41 @@
 <?php 
 
-class Customer {}
+class Customer 
+{
+    private $id;
+    private $firstName;
+    private $lastName;
+    
+    public function setId(int $id) : Customer
+    {
+        $this->id = $id;
+        return $this;
+    }
+    
+    public function setFirstName(string $firstName) : Customer
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+    
+    public function setLastName(string $lastName) : Customer
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+    
+    public function getId() : int
+    {
+        return (int) $this->id;
+    }
+    
+    public function getFirstName() : string
+    {
+        return $this->firstName;
+    }
+    
+    public function geLastName() : string
+    {
+        return $this->lastName;
+    }
+}
