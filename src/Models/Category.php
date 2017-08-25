de7b5b8137f41129572c3746d6c46e39fc5fd2a6
alezvi <alezvi@users.noreply.github.com>
2017-08-24 23:31:30 -0300

    Added attributes, setters and getters.

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -1,3 +1,38 @@
 <?php 
 
-class Category {}
+class Category extends Model
+{
+    
+    private $id;
+    
+    private $name;
+    
+    private $createdAt;
+    
+    private $updatedAt;
+    
+    private $deletedAt;
+    
+    public function setId(int $id) : Category
+    {
+        $this->id = $id;
+        return $this;
+    }
+    
+    public function setName(string $name) : Category
+    {
+        $this->name = name;
+        return $this;
+    }
+    
+    public function getId() : int
+    {
+        return (int) $this->id;
+    }
+    
+    public function getName() : string
+    {
+        return $this->name;
+    }
+    
+}
