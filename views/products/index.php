83d557b8b66848007c9bc0b8d611a5206072ba37
alezvi <alezvi@users.noreply.github.com>
2017-08-24 23:26:05 -0300

    Create index.php

---
+++ views/products/index.php
@@ -0,0 +1,27 @@
+<h1>Products</h1>
+
+<table>
+    
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>Brand</td>
+            <td>Name</td>
+            <td>Price</td>
+            <td>Stock</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+    
+</table>
