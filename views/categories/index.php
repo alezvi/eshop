2a54740bbce719d529d1477b2e1fcd6d69cbbda9
alezvi <alezvi@users.noreply.github.com>
2017-08-25 08:30:12 -0300

    HTML table added.

--- views/categories/index.php
+++ views/categories/index.php
@@ -1,1 +1,28 @@
+<h1>Categories</h1>
 
+<table>
+    
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Total Items</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    
+    <tbody>
+        <?php foreach ($categories as $category): ?>
+            <tr>
+                <td><?= $category->getId() ?></td>
+                <td><?= $category->getName() ?></td>
+                <td></td>
+                <td>
+                    <a href="index.php?route=categories/{$category->getId()}/edit">Edit</a>
+                    <a href="index.php?route=categories/{$category->getId()}/delete">Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+    
+</table>
