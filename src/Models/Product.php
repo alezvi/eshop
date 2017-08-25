91c3ce3fbd49547621022f1a0c0138a1b0898f70
alezvi <alezvi@users.noreply.github.com>
2017-08-25 08:00:05 -0300

    Added attributes, setters and getters.

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -1,3 +1,79 @@
 <?php 
 
-class Product {}
+class Product 
+{
+    
+    private $id;
+    private $name;
+    private $brand;
+    private $stock;
+    private $price;
+    private $photo;
+    
+    public function setId(int $id) : Product 
+    {
+        $this->id = $id;
+        return $this;
+    }
+    
+    public function setBrand(string $name) : Product 
+    {
+        $this->brand = $brand;
+        return $this;
+    }
+    
+    public function setName(string $name) : Product 
+    {
+        $this->name = $name;
+        return $this;
+    }
+    
+    public function setStock(int $stock = 0) : Product 
+    {
+        $this->stock = $stock;
+        return $this;
+    }
+    
+    public function setPrice(float $price = .0) : Product 
+    {
+        $this->price = $price;
+        return $this;
+    }
+    
+    public function setPhoto(string $photo) : Product 
+    {
+        $this->photo = $photo;
+        return $this;
+    }
+    
+    public function getId() : int 
+    {
+        return $this->id;
+    }
+    
+    public function getBrand() : string
+    {
+        return $this->brand;
+    }
+    
+    public function getName() : string
+    {
+        return $this->name;
+    }
+    
+    public function getStock() : int
+    {
+        return (int) $this->stock;
+    }
+    
+    public function getPrice() : float
+    {
+        return (float) $this->price;
+    }
+    
+    public function getPhoto() : string
+    {
+        return $this->photo;
+    }
+    
+}
