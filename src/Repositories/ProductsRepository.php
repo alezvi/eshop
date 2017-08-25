3a8692620b53ac059ab5cd867f2051cec541842d
alezvi <alezvi@users.noreply.github.com>
2017-08-25 08:05:42 -0300

    Filter byId method added.

--- src/Repositories/ProductsRepository.php
+++ src/Repositories/ProductsRepository.php
@@ -1,3 +1,20 @@
 <?php 
 
-class ProductsRepository {}
+class ProductsRepository 
+{
+    
+    private $collection;
+    
+    public function byId($id) 
+    {
+        return array_filter($this->collection, function ($item) use ($id) {
+            if (is_string($id)) {
+                return $item->id == $id;
+            }
+            
+            if (is_array($id)) {
+                return array_key_exists($item->id, $id);
+            }
+        });
+    }
+}
