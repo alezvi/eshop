59c86d68bfd488b3167ddb37518b19ce34e0588d
alezvi <alezvi@users.noreply.github.com>
2017-08-25 08:32:58 -0300

    HTML table added.

--- views/customers/view.php
+++ views/customers/view.php
@@ -1,1 +1,28 @@
-<h1>Customer Info</h1>
+<h1>Customers Info</h1>
+
+<table>
+    
+    <thead>
+        <tr>
+            <td>#</td>
+            <td>Full Name</td>
+            <td>Actions</td>
+        </tr>
+    </thead>
+    
+    <tbody>
+        <?php foreach ($customers as $customer): ?>
+            <tr>
+                <td><?= $customer->getId() ?></td>
+                <td><?= $customer->getFirstName() . ' ' . $customer->getLastName() ?></td>
+                <td></td>
+                <td>
+                    <a href="index.php?route=customers/{$customer->getId()}/edit">Edit</a>
+                    <a href="index.php?route=customers/{$customer->getId()}/delete">Delete</a>
+                    <a href="index.php?route=customers/{$customer->getId()}/orders">Ordenes de Compra</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+    
+</table>
