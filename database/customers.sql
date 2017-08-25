9baef83f47a3d8f58db3a13a9f91e284d1858d95
alezvi <alezvi@users.noreply.github.com>
2017-08-25 14:08:35 -0300

    SQL sentence to create customers table.

--- database/customers.sql
+++ database/customers.sql
@@ -1,1 +1,17 @@
 
+/**
+ * Create customers table
+ */
+
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  
+) 
+ENGINE=InnoDB 
+DEFAULT CHARSET=latin1;
