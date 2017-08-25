d330c1e8d6bb64c8827c4727a55eddec44c0de58
alezvi <alezvi@users.noreply.github.com>
2017-08-24 23:06:52 -0300

    SQL sentence for table creation

--- database/categories.sql
+++ database/categories.sql
@@ -1,1 +1,19 @@
+/**
+ * Create categories table
+ */
 
+CREATE TABLE `categories` (
+    
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+    
+    PRIMARY KEY(`id`)
+    
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=MyISAM
+AUTO_INCREMENT=1
+;
