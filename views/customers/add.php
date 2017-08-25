ba2de0fb9f93c98ab5606263027b6debbb1f6819
alezvi <alezvi@users.noreply.github.com>
2017-08-24 23:22:33 -0300

    Create add.php

---
+++ views/customers/add.php
@@ -0,0 +1,17 @@
+<h1>Customer - Add</h1>
+
+<form method="POST>
+    <div>
+        <label>Firstname</label>
+        <input type="text" name="firstname" />
+    </div>
+    
+    <div>
+        <label>Lastname</label>
+        <input type="text" name="lastname" />
+    </div>
+    
+    <div>
+        <input type="submit" value="Save" />
+    </div>
+</form>
