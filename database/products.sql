dcaa9b4af3d0685455f858aa3788a7d087059879
alezvi <alezvi@users.noreply.github.com>
2017-08-24 23:09:37 -0300

    SQL sentence for table creation.

--- database/products.sql
+++ database/products.sql
@@ -1,1 +1,25 @@
 
+/**
+ * Create products table
+ */
+
+CREATE TABLE `products` (
+    
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `brand` VARCHAR(255) NOT NULL,
+    `price` DOUBLE(7,2) NOT NULL DEFAULT '0',
+    `stock` INT NOT NULL DEFAULT '0',
+    `category_id` INT NOT NULL DEFAULT '0',
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    `deleted_at` TIMESTAMP NULL DEFAULT NULL,
+    
+    PRIMARY KEY(`id`),
+    INDEX `fk_product_belongs_to_category` (`category_id`)
+    
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=MyISAM
+AUTO_INCREMENT=1
+;
