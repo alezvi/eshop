1f17087bbcab4576b036e1b56c83ce02f2f0eea2
alezvi <alezvi@users.noreply.github.com>
2017-08-24 23:17:57 -0300

    Create dev.php

---
+++ config/dev.php
@@ -0,0 +1,29 @@
+<?php 
+
+/**
+ * 
+ * Configuration for development environment
+ * 
+ */
+
+return [
+    
+    'database' => [
+        
+        /**
+         * Default connection. 
+         * 
+         * Replace by your configuration info.
+         */
+         
+        'default' => [
+            'engine' => 'mysql',
+            'host' => 'localhost',
+            'user' => 'root',
+            'password' => 'secret',
+            'database' => 'ecommerce',
+        ],
+        
+    ]
+  
+];
