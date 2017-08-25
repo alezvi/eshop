dd16d5230895bb86942bfd564755011687c283f2
aletopo <av@castinellc.com>
2017-08-25 14:25:50 -0300

    PHP DocBlocks added

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -2,87 +2,163 @@
 
 class Product 
 {
-    
+
+    /**
+     * @var int
+     */
     private $id;
+
+    /**
+     * @var string
+     */
     private $name;
+
+    /**
+     * @var string
+     */
     private $brand;
+
+    /**
+     * @var int
+     */
     private $stock;
+
+    /**
+     * @var float
+     */
     private $price;
+
+    /**
+     * @var string
+     */
     private $photo;
+
+    /**
+     * @var int
+     */
     private $categoryId;
-    
-    public function setId(int $id) : Product 
+
+    /**
+     * @param int $id
+     * @return Product
+     */
+    public function setId(int $id) : Product
     {
         $this->id = $id;
         return $this;
     }
-    
-    public function setBrand(string $name) : Product 
+
+    /**
+     * @param string $brand
+     * @return Product
+     */
+    public function setBrand(string $brand) : Product
     {
         $this->brand = $brand;
         return $this;
     }
-    
-    public function setName(string $name) : Product 
+
+    /**
+     * @param string $name
+     * @return Product
+     */
+    public function setName(string $name) : Product
     {
         $this->name = $name;
         return $this;
     }
-    
-    public function setStock(int $stock = 0) : Product 
+
+    /**
+     * @param int $stock
+     * @return Product
+     */
+    public function setStock(int $stock = 0) : Product
     {
         $this->stock = $stock;
         return $this;
     }
-    
-    public function setPrice(float $price = .0) : Product 
+
+    /**
+     * @param float $price
+     * @return Product
+     */
+    public function setPrice(float $price = .0) : Product
     {
         $this->price = $price;
         return $this;
     }
-    
-    public function setPhoto(string $photo) : Product 
+
+    /**
+     * @param string $photo
+     * @return Product
+     */
+    public function setPhoto(string $photo) : Product
     {
         $this->photo = $photo;
         return $this;
     }
-    
+
+    /**
+     * @param $id
+     * @return Product
+     */
     public function setCategoryId($id) : Product
     {
         $this->categoryId = $id;
         return $this;
     }
-    
-    public function getId() : int 
+
+    /**
+     * @return int
+     */
+    public function getId() : int
     {
         return $this->id;
     }
-    
+
+    /**
+     * @return string
+     */
     public function getBrand() : string
     {
         return $this->brand;
     }
-    
+
+    /**
+     * @return string
+     */
     public function getName() : string
     {
         return $this->name;
     }
-    
+
+    /**
+     * @return int
+     */
     public function getStock() : int
     {
         return (int) $this->stock;
     }
-    
+
+    /**
+     * @return float
+     */
     public function getPrice() : float
     {
         return (float) $this->price;
     }
-    
+
+    /**
+     * @return string
+     */
     public function getPhoto() : string
     {
         return $this->photo;
     }
-    
+
+    /**
+     * @return int
+     */
     public function getCategoryId() : int
     {
         return $this->categoryId;
