bafbf8ba231b1cc58d670a4b5739ee7ec42b5395
alezvi <alezvi@users.noreply.github.com>
2017-08-25 08:08:39 -0300

    Constructor method added.

--- src/Repositories/ProductsRepository.php
+++ src/Repositories/ProductsRepository.php
@@ -5,6 +5,11 @@ class ProductsRepository
     
     private $collection;
     
+    public function __construct() 
+    {
+        $this->collection = new ProductsCollection;
+    }
+    
     public function byId($id) 
     {
         return array_filter($this->collection, function ($item) use ($id) {
