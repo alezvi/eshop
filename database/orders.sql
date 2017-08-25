3b0a0f5b5c31a8b35190a7b71deb785f886a90d5
alezvi <alezvi@users.noreply.github.com>
2017-08-25 14:13:15 -0300

    SQL sentence to create orders table.

--- database/orders.sql
+++ database/orders.sql
@@ -1,1 +1,18 @@
 
+/**
+ * Create the orders table
+ */
+
+CREATE TABLE `orders` (
+	`id` INT(10) UNSIGNED NOT NULL,
+	`customer_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+	`subtotal` INT(11) NULL DEFAULT NULL,
+	`payment_stauts` ENUM('pending','rejected','accepted') NULL DEFAULT 'pending',
+	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+	`deleted_at` TIMESTAMP NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	INDEX `customer_id` (`customer_id`)
+)
+ENGINE=InnoDB
+;
